00c9d75b6aee5f97a2b0fd50e36c935f8f7c86f9
Tfh-Yqf <2813223276tfh@gmail.com>
2023-05-13 21:43:38 +0800

    求旋转角

---
+++ SpringBoot/src/main/java/com/homepainter/util/Angle.java
@@ -0,0 +1,26 @@
+package com.homepainter.util;
+
+public class Angle {
+
+    public static void main(String args[]){
+        double[] a = {0, 1};
+        double[] b = {-3, -1};
+        System.out.println(clockwiseAngle(a,b)*180/3.1415);
+    }
+
+
+    public static double clockwiseAngle(double[] a, double[] b) {
+        // 计算向量的点积
+        double dotProduct = a[0] * b[0] + a[1] * b[1];
+        // 计算向量的叉积
+        double crossProduct = a[0] * b[1] - a[1] * b[0];
+        // 判断向量 b 在向量 a 的顺时针方向还是逆时针方向
+        if (crossProduct < 0) {
+            // b 在 a 的逆时针方向，夹角为负值
+            return -Math.atan2(crossProduct, dotProduct);
+        } else {
+            // b 在 a 的顺时针方向，夹角为正值
+            return Math.atan2(crossProduct, dotProduct);
+        }
+    }
+}
